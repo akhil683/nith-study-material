5f69bb3bbc939bf2da5f3cec0d951550be83dee9
akhil <akhilpalsra@gmail.com>
2023-04-05 01:45:56 +0530

    fixed main for mobile

--- build/script.js
+++ build/script.js
@@ -1,10 +1,12 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
+    const main = document.getElementById('content')
 
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
+        main.classList.toggle('hidden')
     }
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
